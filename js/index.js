22f2cd16edad08199013d5fd9528348e4e20872f
Anastasiia Mets <anastasiia.mets@gmail.com>
2018-09-19 21:11:32 +0000

    dirty words generator

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var randomBodyParts = ["eyes", "head", "mouth", "nose", "leg"];
+var randomAjectives = ["dirty", "sticky", "muddy", "stupid"];
+var randomWords = ["cunt", "penis", "vagina", "sperm"];
+var randomBodyPart = randomBodyParts[Math.floor(Math.random() * randomBodyParts.length)];
+var randomAjective = randomAjectives[Math.floor(Math.random() * randomAjectives.length)];
+var randomWord = randomWords[Math.floor(Math.random() * randomWords.length)];
+var randomInsult = ["You have", randomBodyPart, "looks like", randomAjective, randomWord].join(" ");
+
+document.write(randomInsult);
